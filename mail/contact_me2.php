54a917818fd8cbf8812099ac01b3889b0f9735a0
eric-chung <ew2chung@outlook.com>
2016-01-29 06:06:43 -0500

    portfolio

--- mail/contact_me2.php
+++ mail/contact_me2.php
@@ -1,34 +1,73 @@
 <?php
-$action=$_REQUEST['action'];
-if ($action=="")    /* display the contact form */
-    {
-    ?>
-    <form  action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="submit">
-    Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
-    Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
-    Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
-    <input type="submit" value="Send email"/>
-    </form>
-    <?php
-    }
-else                /* send the submitted data */
-    {
-    $name=$_REQUEST['name'];
-    $email=$_REQUEST['email'];
-    $message=$_REQUEST['message'];
-    if (($name=="")||($email=="")||($message==""))
-        {
-        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-        }
-    else{
-        $from="From: $name<$email>\r\nReturn-path: $email";
-        $subject="Message sent using your contact form";
-        mail("ew2chung@outlook.com", $subject, $message, $from);
-        echo "Email sent!";
-        }
-    }
+if(isset($_POST['email'])) {
+     // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "ew2chung@outlook.com";
+    $email_subject = "Your email subject line";
+
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+
+        die();
+}
+    // validation expected data exists
+    if(!isset($_POST['first_name']) ||
+        !isset($_POST['last_name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['telephone']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');
+
+}
+
+    $first_name = $_POST['first_name']; // required
+    $email_from = $_POST['email']; // required
+    $telephone = $_POST['telephone']; // not required
+    $comments = $_POST['comments']; // required
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+    $email_message = "Form details below.\n\n";
+
+
+    $email_message .= "First Name: ".clean_string($first_name)."\n";
+
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+
+    $email_message .= "Telephone: ".clean_string($telephone)."\n";
+
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+
+
+
+
+
+// create email headers
+
+$headers = 'From: '.$email_from."\r\n".
+
+'Reply-To: '.$email_from."\r\n" .
+
+'X-Mailer: PHP/' . phpversion();
+
+@mail($email_to, $email_subject, $email_message, $headers);
+
+?>
+
+
+
+<!-- include your own success html here -->
+
+
+
+Your message has been sent. I will get back to you within 24 hours. Have a great day!
+
+
+
+<?php
+
+}
+
 ?>
